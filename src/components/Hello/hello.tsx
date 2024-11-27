6cf966ce8bb7bb0f834061d5487cddb027a79a26
ViktorKapra <viktorkapra@gmail.com>
2024-11-27 10:08:16 +0200

    Added Hello section, removed unnecessary files

---
+++ src/components/Hello/hello.tsx
@@ -0,0 +1,10 @@
+import * as styles from "./hello.m.scss";
+
+export default function HelloSection() {
+  return (
+    <section className={styles.helloSection}>
+      <h1>Hello World</h1>
+      <p>just for testing</p>
+    </section>
+  );
+}
